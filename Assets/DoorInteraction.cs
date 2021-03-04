33704ceb3b49952ebd68be3066d7a4d850eaa24a
Michael John Belette O'Brien <mobrie5@g.clemson.edu>
2021-03-04 13:47:24 -0500

    FinishedDoorAndFireExtinguisher

--- Assets/DoorInteraction.cs
+++ Assets/DoorInteraction.cs
@@ -6,18 +6,43 @@ public class DoorInteraction : MonoBehaviour
 {
     //this is a reference to the VRplayer
     public GameObject Door;
+    private bool isdoorHandleOnStill;
     //do not need to know player exists
-    
+
+
+    void Start()
+    {
+        isdoorHandleOnStill = true;
+        
+    }
     // Update is called once per frame
     void Update()
     {
         //each door handle's initial rotaiton will be (0, 0, 0)
         //check to see how much THIS has been rotated. Once it reaches the right point, rotate door to open
-        if (this.gameObject.transform.rotation.x <= -90)
+        if (this.gameObject.transform.rotation.x <= -90 && isdoorHandleOnStill)
         {
             //rotate the door automatically -90 in the y direction
             //think about adding somewhat of a delay...
             Door.transform.rotation = Quaternion.Euler(new Vector3(Door.transform.rotation.x, -90, Door.transform.rotation.z));
+        }
+    }
+
+    //Detect the collision with the fireextinquisher
+    void OnCollisionEnter(Collision collision)
+    {
+        //checking for collision with the fire extinquisher
+        if (collision.gameObject.name == "fireExtinguisher")
+        {
+            //some random number (make it super small to make sure it works
+            if (collision.rigidbody.velocity.magnitude >= .1)
+            {
+                //get the rigidbody of this component
+                Rigidbody rb = GetComponent<Rigidbody>();
+                //turn the gravity on, allowing the handle to fall to the floor
+                rb.useGravity = true;
+                isdoorHandleOnStill = false;
+            }
         }
     }
 }
--- Assets/ExtinguishFireAndBreakOffDoorHandle.cs
+++ Assets/ExtinguishFireAndBreakOffDoorHandle.cs
@@ -5,12 +5,40 @@ using UnityEngine;
 public class ExtinguishFireAndBreakOffDoorHandle : MonoBehaviour
 {
     // Update is called once per frame
+    //for now just make it an int
+    //but how do i get a refernce to one of the buttons on the controller alias...
+    private int buttonValue = 0;
+    private RaycastHit[] hits;
+    private ParticleSystem thisParticle;
+    public GameObject capsuleref;
+
     void Update()
     {
-        //check for a button press indicating that the player is using the fire extinquisher
-            //If the button is pressed, enable and start the particle system
-                //Should out a raycastall to get a list of the colliders
-                    //Loop over the colliders checking to see if they have the tag, "Fire"
-            //once the player let's go of the button, disable the and stop the particle system
+        //check for the button being pressed indicating that the player is using the fire extinquisher
+        if (buttonValue == 0)
+        {
+            //get a reference to the particle system attached to THIS game obejct 
+            thisParticle = GetComponentInChildren<ParticleSystem>();
+
+            //enable the particle system to start (foam is coming out)
+            thisParticle.Play();
+
+            //play around with the arguments here
+            //Shoot out a raycastall to get a list of the colliders
+            hits = Physics.RaycastAll(thisParticle.transform.position, capsuleref.transform.localScale, 100.0F);
+
+            //Loop over the colliders checking to see if they have the tag, "Fire"
+            foreach (RaycastHit hitObject in hits)
+            {
+                //If they do, then delete them from the scene. 
+                if (hitObject.transform.tag == "fire")
+                {
+                    Destroy(hitObject.collider.gameObject);
+
+                }
+            }
+        }
+        //once the player let's go of the button, disable the and stop the particle system
+        thisParticle.Stop();
     }
 }
