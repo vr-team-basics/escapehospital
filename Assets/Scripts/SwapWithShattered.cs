16bddf77931184c5d3a935ccd671555e10f0523b
Armando Partida Sanabia <armandopartida0@gmail.com>
2021-03-10 09:40:39 -0500

    Added window prefab with shatter script * Window prefab (not broken) can be dragged into scene

---
+++ Assets/Scripts/SwapWithShattered.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapWithShattered : MonoBehaviour
+{
+    // Settings for shattering conditions
+    [SerializeField]
+    private GameObject ShatteredVariant;
+
+    [SerializeField]
+    private float MinMassThreshold;
+
+    // Only spawn one shatteredvariant
+    bool spawnedShattered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedShattered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Get the other game object
+        GameObject otherObject = collision.gameObject;
+
+        // Check if other object has Rigidbody
+        if(otherObject.GetComponent<Rigidbody>() != null)
+        {
+            // If other object is dense enough, spawn shattered variant. Only if it hasn't spawned one
+            if (otherObject.GetComponent<Rigidbody>().mass >= MinMassThreshold && !spawnedShattered)
+            {
+                GameObject newObject = Instantiate(ShatteredVariant, transform.position, transform.rotation);
+                newObject.transform.localScale = transform.localScale;
+                spawnedShattered = true;
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
