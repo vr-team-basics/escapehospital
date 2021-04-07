9d54549cd66b465629a95fae48f0ccd81e91767c
MichaelJohn <42548304+MichaelMcshade@users.noreply.github.com>
2021-04-07 18:18:49 -0400

    Final temperature.cs
    Had a few merge conflicts and decided to manually come in and change the code!

--- Assets/Scripts/temperature.cs
+++ Assets/Scripts/temperature.cs
@@ -5,17 +5,22 @@ using static UnityEngine.Random;
 public class temperature : MonoBehaviour
 {
 
-
-    private int heatCapacity;
+    private float[] heatCapacity;
+    //private float heatCapacity;
     private Collider[] hitcolliders;
     public GameObject firePreFab;
     private Mesh mesh;
+    private int verticeAmount;
+    private int randomnumber;
+    //vertices of the mesh
     private Vector3[] vertices;
-    static private int threshold = 2000;
+    private int numberOfFires;
+    private int maxNumFireForThisObject;
+    private float threshold = 1000.0f;
     public GameObject firepropagatingPreFab;
-    private int done;
+    private bool done;
 
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -23,57 +28,118 @@ public class temperature : MonoBehaviour
         //get the mesh this script is attached to and its vertices
         mesh = GetComponent<MeshFilter>().mesh;
         vertices = mesh.vertices;
-        heatCapacity = 0;
-        done = 0;
-    }
+        done = false;
+        numberOfFires = 0;
+        maxNumFireForThisObject = 1;
+        //get the number of vertices
+        verticeAmount = mesh.vertexCount;
+        //this.heatCapacity = 0;
+        //heatCapacity indices correspond to the vertices indices! (tiee the vertices with their unique heatcapacity value!)
+        
+        heatCapacity = new float[verticeAmount];
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            //set initial values for each vertices
+            heatCapacity[i] = 0;
+        }
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Random;
+public class temperature : MonoBehaviour
+{
 
-    void GettingHotter()
+
+    private float[] heatCapacity;
+    //private float heatCapacity;
+    private Collider[] hitcolliders;
+    public GameObject firePreFab;
+    private Mesh mesh;
+    private int verticeAmount;
+    private int randomnumber;
+    //vertices of the mesh
+    private Vector3[] vertices;
+    private int numberOfFires;
+    private int maxNumFireForThisObject;
+    private float threshold = 1000.0f;
+    public GameObject firepropagatingPreFab;
+    private bool done;
+
+    
+    // Start is called before the first frame update
+    void Start()
     {
-        this.heatCapacity += 10;
+        //Set randomly at the beginning of the game
+        //get the mesh this script is attached to and its vertices
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        done = false;
+        numberOfFires = 0;
+        maxNumFireForThisObject = 1;
+        //get the number of vertices
+        verticeAmount = mesh.vertexCount;
+        //this.heatCapacity = 0;
+        //heatCapacity indices correspond to the vertices indices! (tiee the vertices with their unique heatcapacity value!)
+        
+        heatCapacity = new float[verticeAmount];
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            //set initial values for each vertices
+            heatCapacity[i] = 0;
+        }
+    }
+    void GettingHotter(int verticeIndex)
+    {
+         heatCapacity[verticeIndex] += 40.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         //the more fire around you, the higher the chance you go on fire
-        if (done == 0)
+        //for each vertice, we want to look around for fire that is near IT
+        if (numberOfFires <= maxNumFireForThisObject && !done)
         {
-            //for each vertice, we want to look around for fire that is near IT
-            for (var i = 0; i < vertices.Length; i++)
+            hitcolliders = Physics.OverlapSphere(this.transform.position, 5f);
+            for (var i = 0; i < vertices.Length / 2; i++)
             {
                 //check for colliders around that vertice
                 //may have to play around with the range
-                hitcolliders = Physics.OverlapSphere(vertices[i], 2f);
+                //hitcolliders = Physics.OverlapSphere(vertices[i], 5f);
                 foreach (Collider hitColide in hitcolliders)
                 {
                     if (hitColide.tag == "fire")
                     {
-                        GettingHotter();
-                        if (heatCapacity >= threshold && done == 0)
+                        GettingHotter(i);
+                        //once heat amount has reached threshold, do it just once!
+                        if (heatCapacity[i] == threshold && !done)
                         {
-                            StartCoroutine(LetFireSpread(hitColide));
-                            done = 1;
-                            this.tag = "fire";
+                            StartCoroutine(LetFireSpread(hitColide, vertices[i]));
+                            //break out of the first for loop
+                            done = true;
                             break;
                         }
                     }
                 }
-
             }
+
         }
     }
 
-    private IEnumerator LetFireSpread(Collider hitColide)
+    private IEnumerator LetFireSpread(Collider hitColide, Vector3 verticeIndex)
     {
         int value = 3;
         while (SpreadBetweenTwoPoints(hitColide.transform.position, this.transform.position, value))
         {
             value -= 1;
-            yield return new WaitForSeconds(.5f);
+            yield return new WaitForSeconds(2f);
         }
+        
         //just use normal fire prefab now!
         Instantiate(firepropagatingPreFab, this.transform.position, this.transform.rotation);
 
+       // Instantiate(firepropagatingPreFab, verticeIndex, this.transform.rotation);
+
     }
 
 
@@ -97,60 +163,21 @@ public class temperature : MonoBehaviour
         //get the difference in each axis
         Vector3 difference = DistanceBetweenTheTwoInEachAxis(initial, final);
 
-        //are they negative or positive
-        incrementX = difference.x / value;
-        incrementY = difference.y / value;
-        incrementZ = difference.z / value;
+ 
         if (value != 0)
         {
+            //are they negative or positive
+            incrementX = difference.x / value;
+            incrementY = difference.y / value;
+            incrementZ = difference.z / value;
+
             Instantiate(firepropagatingPreFab, new Vector3(initial.x + incrementX, initial.y + incrementY, initial.z + incrementZ), this.transform.rotation);
             return true;
         }
         return false;
     }
-
-    //ORIGINAL COPY INCASE SOMETHING HAPPENS
-    /*
-    void GettingHotter()
-    {
-        this.heatCapacity += 10.0f;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //the more fire around you, the higher the chance you go on fire
-        if (done == 0)
-        {
-            hitcolliders = Physics.OverlapSphere(this.transform.position, 2f);
-            foreach (Collider hitColide in hitcolliders)
-            {
-                if (hitColide.tag == "fire")
-                {
-                    GettingHotter();
-                    if (heatCapacity >= threshold && done == 0)
-                    {
-                        StartCoroutine(LetFireSpread(hitColide));
-                        done = 1;
-                        this.tag = "fire";
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    private IEnumerator LetFireSpread(Collider hitColide)
-    {
-        int value = 3;
-        while (SpreadBetweenTwoPoints(hitColide.transform.position, this.transform.position, value))
-        {
-            value -= 1;
-            yield return new WaitForSeconds(.5f);
-        }
-        Instantiate(firePreFab, this.transform.position, this.transform.rotation);
-        
-    }
+    
+}
 
 
 
@@ -173,16 +200,17 @@ public class temperature : MonoBehaviour
         //get the difference in each axis
         Vector3 difference = DistanceBetweenTheTwoInEachAxis(initial, final);
 
-        //are they negative or positive
-        incrementX = difference.x / value;
-        incrementY = difference.y / value;
-        incrementZ = difference.z / value;
+ 
         if (value != 0)
         {
+            //are they negative or positive
+            incrementX = difference.x / value;
+            incrementY = difference.y / value;
+            incrementZ = difference.z / value;
+
             Instantiate(firepropagatingPreFab, new Vector3(initial.x + incrementX, initial.y + incrementY, initial.z + incrementZ), this.transform.rotation);
             return true;
         }
         return false;
     }
-    */
 }
