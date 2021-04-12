9cbbef3b06ea91734d74eaf2a8b7dfab402c18f1
Michael John Belette O'Brien <mobrie5@g.clemson.edu>
2021-04-12 16:03:56 -0400

    HadToFixFire
    Some problem from when I transferred the file between projects last week

--- Assets/Scripts/temperature.cs
+++ Assets/Scripts/temperature.cs
@@ -1,47 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using static UnityEngine.Random;
-public class temperature : MonoBehaviour
-{
 
-    private float[] heatCapacity;
-    //private float heatCapacity;
-    private Collider[] hitcolliders;
-    public GameObject firePreFab;
-    private Mesh mesh;
-    private int verticeAmount;
-    private int randomnumber;
-    //vertices of the mesh
-    private Vector3[] vertices;
-    private int numberOfFires;
-    private int maxNumFireForThisObject;
-    private float threshold = 1000.0f;
-    public GameObject firepropagatingPreFab;
-    private bool done;
-
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Set randomly at the beginning of the game
-        //get the mesh this script is attached to and its vertices
-        mesh = GetComponent<MeshFilter>().mesh;
-        vertices = mesh.vertices;
-        done = false;
-        numberOfFires = 0;
-        maxNumFireForThisObject = 1;
-        //get the number of vertices
-        verticeAmount = mesh.vertexCount;
-        //this.heatCapacity = 0;
-        //heatCapacity indices correspond to the vertices indices! (tiee the vertices with their unique heatcapacity value!)
-        
-        heatCapacity = new float[verticeAmount];
-        for (var i = 0; i < vertices.Length; i++)
-        {
-            //set initial values for each vertices
-            heatCapacity[i] = 0;
-        }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -181,36 +138,4 @@ public class temperature : MonoBehaviour
 
 
 
-    Vector3 DistanceBetweenTheTwoInEachAxis(Vector3 initial, Vector3 final)
-    {
-        //Get the distance between each x, y, z of the two vectors!
-        Vector3 distance;
-        distance.x = final.x - initial.x;
-        distance.y = final.y - initial.y;
-        distance.z = final.z - initial.z;
-        return distance;
-    }
-    bool SpreadBetweenTwoPoints(Vector3 initial, Vector3 final, int value)
-    {
-        //the distance between any two fires will always be below 5.0f (BECAUSE OF THE RADIUS YOU HAVE NOW) THIS CAN BE CHANGED THOUGH SO BE CAREFUL!
-        float incrementX = 0.0f;
-        float incrementY = 0.0f;
-        float incrementZ = 0.0f;
 
-        //get the difference in each axis
-        Vector3 difference = DistanceBetweenTheTwoInEachAxis(initial, final);
-
- 
-        if (value != 0)
-        {
-            //are they negative or positive
-            incrementX = difference.x / value;
-            incrementY = difference.y / value;
-            incrementZ = difference.z / value;
-
-            Instantiate(firepropagatingPreFab, new Vector3(initial.x + incrementX, initial.y + incrementY, initial.z + incrementZ), this.transform.rotation);
-            return true;
-        }
-        return false;
-    }
-}
